45efa2751bededd8778c1599410319fd98699401
Mustafa Aziz Sevimli <mustafaazizsevimli@gmail.com>
2026-09-21 23:08:15 +0300

    feat: create subcategories table and add subcategories modules

--- src/app.js
+++ src/app.js
@@ -4,15 +4,23 @@ import dotenv from "dotenv";
 import authRoutes from "./modules/auth/auth.routes.js";
 import storeRoutes from "./modules/store/store.routes.js";
 import categoryRoutes from "./modules/category/category.routes.js";
+import subcategoryRoutes from "./modules/subcategory/subcategory.routes.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API çalışıyor!");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/store", storeRoutes);
 app.use("/api/category", categoryRoutes);
+app.use("/api/subcategory", subcategoryRoutes);
 
 app.listen(port, () => {
   console.log(`Sunucu http://localhost:${port} üzeinde aktif`);
